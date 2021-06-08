ab915860556402a994938d6555f12e0a8a7e15c0
UmmuIzzatulKharimah <ummu.izzatul.tif19@polban.ac.id>
2021-06-09 00:38:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sms/SMSReceiver.java
@@ -0,0 +1,44 @@
+package com.example.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String str = "SMS from ";
+        if (bundle != null)
+        {
+
+            msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+
+            for (int i=0; i<msgs.length; i++){
+                str += msgs[i].getMessageBody().toString();
+            }
+
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+            Log.d("SMSReceiver", str);
+
+
+            Intent mainActivityIntent = new Intent(context, MainActivity.class);
+            mainActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(mainActivityIntent);
+
+            Intent broadcastIntent = new Intent();
+            broadcastIntent.setAction("SMS_RECEIVED_ACTION");
+            broadcastIntent.putExtra("sms", str);
+            context.sendBroadcast(broadcastIntent);
+
+        }
+    }
+}
